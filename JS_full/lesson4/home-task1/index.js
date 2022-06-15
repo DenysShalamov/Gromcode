e55d552cd00b6f6446459c563328d4f43b3b51d2
Denys Shalamov <shalamov.denys@gmail.com>
2022-06-15 16:38:25 +0300

    asd

--- JS_full/lesson4/home-task1/index.js
+++ JS_full/lesson4/home-task1/index.js
@@ -1,15 +1,15 @@
 function run() {
-  var a = 0;
+  const a = 0;
 
-  if (a == 1) {
-    return c;
+  if (a === 1) {
+    return 10;
   }
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 10; i += 1) {
     console.log(i);
   }
 
-  return (d = a);
+  return a;
 }
 
 run();
