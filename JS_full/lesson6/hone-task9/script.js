37340c5dded8a316ca6b273a604955bbeb759a07
Denys Shalamov <shalamov.denys@gmail.com>
2022-06-16 18:16:02 +0300

    lesson 6

---
+++ JS_full/lesson6/hone-task9/script.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-param-reassign */
+const withdraw = function (clients, balances, client, amount) {
+  if (balances[clients.indexOf(client)] - amount < 0) {
+    return -1;
+  }
+  balances[clients.indexOf(client)] -= amount;
+  return balances[clients.indexOf(client)];
+};
+
+console.log(withdraw(["Ann", "John", "User"], [1400, 87, -6], "John", 50));
