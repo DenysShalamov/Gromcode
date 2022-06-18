4b3c52d25e4f6f1e63c13c9574515a50c794d618
Denys Shalamov <shalamov.denys@gmail.com>
2022-06-18 12:57:42 +0300

    lesson7

--- JS_full/lesson7/lesson-task3/script.js
+++ JS_full/lesson7/lesson-task3/script.js
@@ -1,8 +1,4 @@
-const sortDesc = (numbers) => {
-  const newArr = [...numbers];
-
-  return newArr.sort((a, b) => b - a);
-};
+const sortDesc = (numbers) => numbers.slice().sort((a, b) => b - a);
 
 const arr = [1, 4, 7, 5, 2, 8, 6];
 
