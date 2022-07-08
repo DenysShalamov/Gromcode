6546a505edb80c58a0a8625288d7f9db323389af
Denys Shalamov <shalamov.denys@gmail.com>
2022-07-08 23:20:06 +0300

    changed

--- JS_full/lesson21/home-task4/index.js
+++ JS_full/lesson21/home-task4/index.js
@@ -1,7 +1,5 @@
-export const setTitle = function (text) {
-  if (typeof text === 'string') {
-    document.querySelector('.title').textContent = text;
-  }
+export const setTitle = function (text = '') {
+  document.querySelector('.title').textContent = text;
 };
 
 console.dir(setTitle('sdfgdfg'));
