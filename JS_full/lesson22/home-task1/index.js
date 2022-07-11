528365f40df6f776c7e556ee232e6427f896e51d
Denys Shalamov <shalamov.denys@gmail.com>
2022-07-11 21:47:48 +0300

    lesson 22 home task 1

--- JS_full/lesson22/home-task1/index.js
+++ JS_full/lesson22/home-task1/index.js
@@ -1,53 +1,9 @@
-const rectDivElem = document.querySelector('.rect_div');
-const rectPElem = document.querySelector('.rect_p');
-const rectSpanElem = document.querySelector('.rect_span');
-const eventsListElem = document.querySelector('.events-list');
-const clearBtn = document.querySelector('.clear-btn');
-const removeBtn = document.querySelector('.remove-handlers-btn');
-const attachBtn = document.querySelector('.attach-handlers-btn');
+const btnElem = document.querySelector('.create-btn');
 
-const logTarget = function (text, color) {
-  eventsListElem.innerHTML += `<span style= "color: ${color}; margin-left: 8px"">${text}</span>`;
+const alert = function () {
+  alert('done');
 };
 
-const logGreenDiv = () => logTarget('div', 'green');
-const logGreenP = () => logTarget('p', 'green');
-const logGreenSpan = () => logTarget('span', 'green');
+const alertEvent = () => alert();
 
-const logGrayDiv = () => logTarget('div', 'gray');
-const logGrayP = () => logTarget('p', 'gray');
-const logGraySpan = () => logTarget('span', 'gray');
-
-const attach = function () {
-  rectDivElem.addEventListener('click', logGreenDiv);
-  rectPElem.addEventListener('click', logGreenP);
-  rectSpanElem.addEventListener('click', logGreenSpan);
-
-  rectDivElem.addEventListener('click', logGrayDiv, true);
-  rectPElem.addEventListener('click', logGrayP, true);
-  rectSpanElem.addEventListener('click', logGraySpan, true);
-};
-
-attach();
-
-const clear = function () {
-  eventsListElem.textContent = '';
-};
-const clearEventList = () => clear();
-clearBtn.addEventListener('click', clearEventList);
-
-const remove = function () {
-  rectDivElem.removeEventListener('click', logGreenDiv);
-  rectPElem.removeEventListener('click', logGreenP);
-  rectSpanElem.removeEventListener('click', logGreenSpan);
-
-  rectDivElem.removeEventListener('click', logGrayDiv, true);
-  rectPElem.removeEventListener('click', logGrayP, true);
-  rectSpanElem.removeEventListener('click', logGraySpan, true);
-};
-const removeEvents = () => remove();
-removeBtn.addEventListener('click', removeEvents);
-
-const attachEvents = () => attach();
-
-attachBtn.addEventListener('click', attachEvents);
+btnElem.addEventListener('click', alertEvent());
