63ec2b12f63ba051b4a205e89edceb1459442df6
Denys Shalamov <shalamov.denys@gmail.com>
2022-07-11 11:17:57 +0300

    lesson 21 home task 7

---
+++ JS_full/lesson21/home-task7/index.js
@@ -0,0 +1,8 @@
+export const creareButton = function (buttonText) {
+  const bodyElem = document.querySelector('body');
+  const newButton = document.createElement('button');
+  newButton.textContent = buttonText;
+  bodyElem.append(newButton);
+};
+
+// creareButton('my button');
