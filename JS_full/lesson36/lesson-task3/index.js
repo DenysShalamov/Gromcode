490e5038330b0139b6134b7356b217dd390ddf9a
Denys Shalamov <shalamov.denys@gmail.com>
2022-07-27 17:15:28 +0300

    changed

--- JS_full/lesson36/lesson-task3/index.js
+++ JS_full/lesson36/lesson-task3/index.js
@@ -1,19 +1,19 @@
-const getUsersBlogs = async userName => {
+const fetchUser = async user => {
   try {
-    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const response = await fetch(`https://api.github.com/users/${user}`);
     if (!response.ok) {
       return null;
     }
     const { blog } = await response.json();
     return blog;
   } catch (err) {
-    throw new Error('Failed to fetch user');
+    throw new Error('Failed to get user data');
   }
 };
 
-const asyncUsers = (...users) => {
-  const promises = users.map(user => getUsersBlogs(user));
+const getUsersBlogs = users => {
+  const promises = users.map(user => fetchUser(user));
   return Promise.all(promises);
 };
 
-asyncUsers('facebook', 'twitter', 'linkedin').then(result => console.log(result));
+getUsersBlogs(['google', 'facebook', 'reactjs']).then(result => console.log(result));
