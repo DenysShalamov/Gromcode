9dcb798eec181e14b89a30b0f61938ca7d4184e0
Denys Shalamov <shalamov.denys@gmail.com>
2022-07-27 21:13:45 +0300

    changed

--- JS_full/lesson34/lesson-task1/src/scripts/index.js
+++ JS_full/lesson34/lesson-task1/src/scripts/index.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
     renderTasks(); // при загрузке страници рендерим таски на страницу (если есть на сервере)
   });
 
-  initTodoListHandlers();
+  initTodoListHandlers(); // запускаем обработчики
 });
-
+// кобэк для ивента на сторэдж
 const onStorageChange = e => {
   if (e.key === 'tasksList') {
     renderTasks();
   }
 };
-window.addEventListener('storage', onStorageChange);
+window.addEventListener('storage', onStorageChange); // обработчик для сторэджа
